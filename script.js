d31ee2dc33719985cda0fd503e6e1bb7ca7d9f06
DeekshaGangwar7251 <gangwardeeksha2301@gmail.com>
2025-08-16 15:29:56 +0530

    modift again

--- script.js
+++ script.js
@@ -22,12 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
-const hamburger = document.getElementById("hamburger");
-const navLinks = document.getElementById("nav-links");
 
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("show");
-});
 
 
 
