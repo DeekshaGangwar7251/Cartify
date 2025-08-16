5a58238c344db64f1a62c0f77f493904f3562228
DeekshaGangwar7251 <gangwardeeksha2301@gmail.com>
2025-08-16 15:25:47 +0530

    modify index,style,js file

--- script.js
+++ script.js
@@ -22,6 +22,12 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
 
 
 
